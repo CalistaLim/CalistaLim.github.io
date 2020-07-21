c7d211590fe30b887f7b5ca3b95430bfa4dd5d94
Ubuntu <ubuntu@ip-172-31-52-16.ec2.internal>
2020-07-21 16:36:41 +0000

    going live

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+            var circle;
+            var circles = [];
+            
         // TODO 2 : Create a function that draws a circle 
-        
-
+           function drawCircle() {
+                circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+                physikz.addRandomVelocity(circle, canvas);
+                view.addChild(circle);
+                circles.push(circle);   
+           }
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+            for (var i = 0; i <= 100; i++){
+                drawCircle();
+           }
+          
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,14 +46,23 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            // for (var i = 0; i < circle.length; i++){
+            //     physikz.updatePosition( circles[i] );
+                
+            // }
+        
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
-           
+        //   for (var i = 0; i < circle.length; i++){
+        //       game.checkCirclePosition( circles[i] );
+        //   }
 
             // TODO 8 : Iterate over the array
-           
-            
+           for (var i = 0; i < circles.length; i++){
+                var eachCircle = circles[i];
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);
+           }
         }
     
         /* 
@@ -62,7 +78,17 @@ var init = function (window) {
             }
             
             // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+            if ( circle.x < 0){
+                circle.x = canvas.width
+            }
             
+            if ( circle.y < 0){
+                circle.y = canvas.height
+            }
+            
+            if ( circle.y > canvas.height){
+                circle.y = 0;
+            }
 
 
             // YOUR TODO 5 CODE ENDS HERE //////////////////////////
@@ -81,7 +107,7 @@ var init = function (window) {
         game.update = update;
         
         app.addUpdateable(window.opspark.game);
-    }
+    };
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
